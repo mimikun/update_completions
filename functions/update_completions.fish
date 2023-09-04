16829b6b36834e8ec2350b6ae109cbb4ef90bda2
Yuto Tanaka <karaage_nennen@icloud.com>
2023-09-04 21:07:01 +0900

    Add rtx completion

--- functions/update_completions.fish
+++ functions/update_completions.fish
@@ -27,8 +27,8 @@ function update_completions --description 'Update completions'
         bun completions >/dev/null 2>&1
     end
 
-    echo "Update: chezmoi, flyctl, bin and runme completions"
-    for cmd in chezmoi flyctl runme bin
+    echo "Update: chezmoi, flyctl, bin, rtx and runme completions"
+    for cmd in chezmoi flyctl runme bin rtx
         if command_exist $cmd
             $cmd completion fish > $completions_dir/$cmd.fish
         end
