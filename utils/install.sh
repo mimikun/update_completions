63c1cdba79e9af5577cf5226b908c82c63726bd5
Yuto Tanaka <karaage_nennen@icloud.com>
2024-05-15 22:50:14 +0900

    fix: install script

--- utils/install.sh
+++ utils/install.sh
@@ -17,4 +17,4 @@ before_sudo
 run_install update_bat_fish_completion
 run_install update_fish_completions
 run_install update_hyperfine_fish_completion
-run_install update_pastel_fish_completion.sh
+run_install update_pastel_fish_completion
